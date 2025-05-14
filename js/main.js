1a863098bc8b882160cab8c4061a8ae79a3202eb
Malak-Essam <63660283+Malak-Essam@users.noreply.github.com>
2025-05-14 20:52:35 +0300

    finished the FAQ section

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// FAQ
+document.addEventListener("DOMContentLoaded", () => {
+    const faqContent = document.querySelector('.faq-content');
+    faqContent.addEventListener('click', (e) => {
+       const groupHeader = e.target.closest('.faq-group-header');
+       if(!groupHeader) return;
+
+       const group = groupHeader.parentElement;
+       const groupBody = group.querySelector('.faq-group-body');
+       const icon = groupHeader.querySelector('i');
+        // toggle icon
+        icon.classList.toggle('fa-plus')
+        icon.classList.toggle('fa-minus')
+        // toggle visability of body
+        groupBody.classList.toggle('open')
+    });
+});
