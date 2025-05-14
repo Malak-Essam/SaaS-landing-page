479666736a12883832291aa0eb7d9dfe76fe1411
Malak-Essam <63660283+Malak-Essam@users.noreply.github.com>
2025-05-15 00:29:45 +0300

    update the css making the project responsive and adding the user-menu button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-// FAQ
+
 document.addEventListener("DOMContentLoaded", () => {
+    // FAQ
     const faqContent = document.querySelector('.faq-content');
     faqContent.addEventListener('click', (e) => {
        const groupHeader = e.target.closest('.faq-group-header');
@@ -13,5 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
         icon.classList.toggle('fa-minus')
         // toggle visability of body
         groupBody.classList.toggle('open')
+    });
+    // mobile menu
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    hamburgerButton.addEventListener('click', (e) => {
+        //toggle active mobile menu
+        const hamburgerMenu = document.querySelector('.mobile-menu')
+        hamburgerMenu.classList.toggle('active')
     });
 });
